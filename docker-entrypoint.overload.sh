1b69682ee28f858e188df6aaa82e8f339f4adf58
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-05-23 16:47:06 +0200

    allow webdav server (daemon) to edit/modify files

--- docker-entrypoint.overload.sh
+++ docker-entrypoint.overload.sh
@@ -6,6 +6,8 @@ if [ "${1#-}" != "$1" ]; then
 	set -- apache2-foreground "$@"
 fi
 # Fix permissions : avoid different user:group in the exposed directory
-chown -R www-data:www-data /var/www/html
+chown -R www-data:daemon /var/www/html
+# to allow webdav server to modify all files
+chmod -R 770 /var/www/html
 
 exec "$@"
